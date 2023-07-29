4a9b38d3df6909b4225c54772b5e2b02b20d8d57
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-07-29 00:14:33 -0700

    Initial commit

---
+++ Razor+_layout/controller/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Razor__layout.controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        } 
+    }
+}
